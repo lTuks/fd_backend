668b7015e3293510e03ff35632c820236b5aa4e9
Vinícius Santos Lopo Vieira <vsantos.lvieira@gmail.com>
2024-04-26 21:08:26 -0300

    Corrigindo gunicorn

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec gunicorn -k uvicorn.workers.UvicornWorker -c gunicorn_config.py app.main:new_facility
+exec gunicorn -k uvicorn.workers.UvicornWorker -c gunicorn_config.py app.main:app
